65a387bd3ac06b88d9a5d03a9e265acd315b56ec
Andy Chen <asjchen15@gmail.com>
2017-10-09 02:33:18 -0700

    Deleted extraneous comments from problem 8

--- src/lib/p0008.rs
+++ src/lib/p0008.rs
@@ -1,15 +1,7 @@
 // Problem 8
 // Largest product in a series
 
-
-
-
-
-
-
 fn largest_consec_digit_product(num_digits: usize) -> i64 {
-
-    // obtain the array of digits somehow
     let large_num_parts: [&'static str; 20] = [
         "73167176531330624919225119674426574742355349194934",
         "96983520312774506326239578318016984801869478851843",
